ffc21f3b419f29447c93d7edb0111972fafbeefa
mukul423 <mukul.indian@gmail.com>
2023-04-16 11:11:48 +0530

    first commit

---
+++ src/main/java/com/fc/mukul/MukulApplication.java
@@ -0,0 +1,13 @@
+package com.fc.mukul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fc/mukul/MukulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.mukul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
